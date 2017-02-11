1816da4801c73d46c7e0bbb6b0919ef16ecaad61
José Rafael Ramón Navarro <jrafa.ramon@gmail.com>
2017-02-11 12:47:21 +0100

    Initial Commit with Repository Pattern

---
+++ Repository/Repository/IGenericRepository.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+
+namespace Repository
+{
+    public interface IGenericRepository<TEntity> where TEntity : class
+    {
+        IEnumerable<TEntity> GetAllEntities();
+        TEntity GetById(int id);
+        void Insert(TEntity entity);
+        void Delete(TEntity entity);
+        void Update(TEntity entity);
+    }
+}
